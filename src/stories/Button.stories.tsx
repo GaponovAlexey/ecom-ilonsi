37c1a4183b61239e4638ab68b5a1f7d252920ec9
Kekelberi <alexei.gaponov@gmail.com>
2022-05-27 20:41:01 +0300

    save

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -4,24 +4,17 @@ import { ComponentStory, ComponentMeta } from '@storybook/react'
 export default {
   title: 'Components/Button',
   component: Button,
-} as ComponentMeta<typeof Button>
-
+}
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
 
-
-export const Red = Template.bind({})
-
-Red.args = {
-  backgroundColor: 'red',
-  label: 'Press Me',
-  size: 'md',
-}
+export const Primary = Template.bind({})
+Primary.args = { backgroundColor: '#ff0', label: 'Button' }
 
 export const Silver = Template.bind({})
 
 Silver.args = {
-  backgroundColor: 'gray',
-  label: 'silver',
+  backgroundColor: 'silver',
+  label: 'Press Me',
   size: 'md',
 }
