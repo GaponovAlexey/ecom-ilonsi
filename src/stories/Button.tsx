bfffd699b933d6892ae0676731d6d4ba42dbadb5
Kekelberi <alexei.gaponov@gmail.com>
2022-05-29 13:50:17 +0300

    save

---
+++ src/stories/Button.tsx
@@ -0,0 +1,23 @@
+import pt from 'prop-types'
+
+function Button({ label = 'Button', backgroundColor = 'red', size = 'md' }) {
+  let scale = 1
+  if (size === 'sm') scale = 0.75
+  if (size === 'lg') scale = 1.5
+  const style = {
+    backgroundColor,
+    padding: `${scale * 0.5}rem ${scale * 1}rem`,
+    border: 'none',
+  }
+  return <button style={style}>{label}</button>
+}
+
+Button.pt = {
+  label: pt.string,
+  args: pt.shape({}),
+  backgroundColor: pt.string,
+  size: pt.oneOf(['sm', 'md', 'lg']),
+  handleClick: pt.func,
+}
+
+export default Button
