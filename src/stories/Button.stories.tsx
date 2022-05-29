17159a9c44939ac5a4e46a6822c327bf745fc832
Kekelberi <alexei.gaponov@gmail.com>
2022-05-29 16:51:47 +0300

    save

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -4,10 +4,6 @@ import { ComponentStory, ComponentMeta } from '@storybook/react'
 export default {
   title: 'Components/Button',
   component: Button,
-  parameters: {
-    zeplinLink:
-      'zpl://components?pid=62927166afb23d1511bff773&coid=6292716a1a8dbc1219fc647f',
-  },
 } as ComponentMeta<typeof Button>
 
 const Template: ComponentStory<typeof Button> = (args) => (
@@ -18,10 +14,6 @@ export const Primary = Template.bind({})
 Primary.args = {
   label: 'Primary',
   backgroundColor: 'blue',
-}
-Primary.parameters = {
-  zeplinLink:
-    'zpl://screen?pid=6292627ef98cc317986e5c52&sid=62926281bcdeda13bcda8b61',
 }
 
 export const Silver = Template.bind({})
